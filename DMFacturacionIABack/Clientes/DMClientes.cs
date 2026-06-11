f2cafea6a855eeb46bf09b24f5f38b06698c265d
Alvaro Restrepo <alvarorz@algarti.com>
2026-06-11 11:54:11 -0500

    Agregar archivos de proyecto.

---
+++ DMFacturacionIABack/Clientes/DMClientes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMFacturacionIABack.Clientes
+{
+    public class DMClientes
+    {
+        private readonly string connectionstring;
+
+
+    }
+}
---
+++ FacturacionIABack/Controllers/ClientesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FacturacionIABack.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientesController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return Ok("dato");
+        }
+    }
+}
