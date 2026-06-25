2c8f990548ff728cbbf20ff212ea7a4329039d9f
AlvaroRestrepo2025 <alvarorz@positivosmais.com>
2026-06-25 10:58:32 -0500

    Update Script BD Seguridad.sql
    Se cambia el tamaño de la columna notas

--- ScriptsSQL/Script BD Seguridad.sql
+++ ScriptsSQL/Script BD Seguridad.sql
@@ -1,4 +1,4 @@
 USE db_SecurityPositivo
 GO
-ALTER TABLE Usuarios ADD Notas VARCHAR(100) --no poner not null
+ALTER TABLE Usuarios ADD Notas VARCHAR(200) --no poner not null
 --no crear columna estado sino usar columna activo
